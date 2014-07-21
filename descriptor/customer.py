7120504e95a333a33a526665eb2ce7fb4a1d26c0
Luciano Ramalho <luciano@ramalho.org>
2014-07-21 01:01:59 -0700

    customer NonBlank descriptor example

--- descriptor/customer.py
+++ descriptor/customer.py
@@ -6,14 +6,15 @@ A client with name and e-mail:
     'John Robinson <jack@rob.org>'
 
 A client with blank e-mail:
-    
-    >>> joe = Customer('Joseph Blow', '')
 
+    >>> joe = Customer('Joseph Blow', '')
+    >>> joe.full_email()
+    'Joseph Blow <>'
 
 """
 
 class Customer:
-    
+
     def __init__(self, name, email, fidelity=0):
         self.name = name
         self.email = email
--- descriptor/customer2.py
+++ descriptor/customer2.py
@@ -5,10 +5,50 @@ A client with name and e-mail:
     >>> jack.full_email()
     'John Robinson <jack@rob.org>'
 
+A client cannot be created with a blank e-mail:
+
+    >>> joe = Customer('Joseph Blow', '')
+    Traceback (most recent call last):
+      ...
+    ValueError: email must not be empty
+    >>> joe = Customer('Joseph Blow', 'joe@blow.up')
+    >>> joe.full_email()
+    'Joseph Blow <joe@blow.up>'
+
+Assigning a blank e-mail later is not allowed either:
+
+    >>> joe.email = ''
+    Traceback (most recent call last):
+      ...
+    ValueError: email must not be empty
+
+`NonBlank` fields must also be strings:
+
+    >>> dummy = Customer(99, 'test@example.com')
+    Traceback (most recent call last):
+      ...
+    TypeError: 'name' must be of type 'str'
+
 """
 
+class NonBlank:
+
+    def __init__(self, storage_name):
+        self.storage_name = storage_name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise TypeError("%r must be of type 'str'" % self.storage_name)
+        elif len(value) == 0:
+            raise ValueError('%s must not be empty' % self.storage_name)
+        instance.__dict__[self.storage_name] = value
+
+
 class Customer:
-    
+
+    name = NonBlank('name')
+    email = NonBlank('email')
+
     def __init__(self, name, email, fidelity=0):
         self.name = name
         self.email = email
